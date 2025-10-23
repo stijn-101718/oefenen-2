cc9866b5710e4566c875b4382765ddc67489fff0
stijn <101718@glr.nl>
2025-10-23 18:22:50 +0000

    prima

--- oefenen.js
+++ oefenen.js
@@ -1,6 +1,10 @@
-function tekst() {
-    const hoi = "Hallo allemaal!";
-    return hoi;
-}
+let raw = prompt("Hoe oud ben je?");
+let leeftijd = Number(raw);
 
-console.log(tekst());
+if (leeftijd >= 65) {
+  console.log("Je mag stemmen — proficiat! (speciale boodschap voor 65+)");
+} else if (leeftijd >= 18) {
+  console.log("Je mag stemmen.");
+} else {
+  console.log("Je mag nog niet stemmen.");
+}
