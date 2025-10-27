94dbefb985623a95a6c7fe00fb3454acbac1cd2a
stijn <101718@glr.nl>
2025-10-27 19:05:06 +0000

    prima

--- oefenen.js
+++ oefenen.js
@@ -1,10 +1,3 @@
-let raw = prompt("Hoe oud ben je?");
-let leeftijd = Number(raw);
-
-if (leeftijd >= 65) {
-  console.log("Je mag stemmen — proficiat! (speciale boodschap voor 65+)");
-} else if (leeftijd >= 18) {
-  console.log("Je mag stemmen.");
-} else {
-  console.log("Je mag nog niet stemmen.");
-}
+// CommonJS
+const data = require('./data.json');
+document.body.innerHTML(data) 
