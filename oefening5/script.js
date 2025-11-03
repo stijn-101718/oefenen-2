6ce7c6a48d222fce5025cf00844cc3e6da98ae5d
stijn <101718@glr.nl>
2025-11-03 12:28:35 +0000

    prima

---
+++ oefening5/script.js
@@ -0,0 +1,8 @@
+let completeNaam = "Stijn";
+let hobbies = "voetbal, gamen, lezen";
+let series = "The Last of Us, Breaking Bad, La Casa de Papel";
+
+let mijnIntroductie = `Mijn naam is ${completeNaam}. Mijn hobby's zijn ${hobbies}. Mijn favoriete series zijn ${series}.`;
+
+console.log(mijnIntroductie);
+
