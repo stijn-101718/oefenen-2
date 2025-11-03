2e5c3b2744e92d6d26ff9f5bc06162f262cb8adb
stijn <101718@glr.nl>
2025-11-03 12:22:08 +0000

    oke

---
+++ oefening4/script.js
@@ -0,0 +1,8 @@
+let myNumber = 18;
+let myString = "Stijn"
+let myBoolean = true;
+
+
+console.log("my number is equal to" + myNumber); 
+console.log("my number is equal to" + myString); 
+console.log("my number is equal to" + myBoolean); 
